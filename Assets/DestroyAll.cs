075ce90955b7d2b39dac10bea2cef1ecc17b48a1
NOT <Panupong.not@hotmail.com>
2017-02-20 16:17:39 +0700

    Initialize Project

---
+++ Assets/DestroyAll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAll : MonoBehaviour {
+
+	private GameObject[] list;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Example());
+
+	}
+
+	IEnumerator Example() {
+		
+		yield return new WaitForSeconds(1);
+		list = GameObject.FindGameObjectsWithTag ("Enemy");
+		StartCoroutine(Example());
+		for (int i = 0; i < list.Length; i++) {
+			Destroy (list [i]);
+
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject fireBall;
+	public GameObject fireBomb;
+	//public Transform shotSpawn;
+
+	private float nextFire;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			Instantiate (fireBall, Input.mousePosition);
+			nextFire = Time.time + 0.25f;
+		}
+	}
+}
